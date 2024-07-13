5eb7c092ef902951cb2ca5ecc98870accfe9c1f0
wangqing <1028431602@qq.com>
2024-07-14 00:40:25 +0800

    feat: layout布局

--- src/router/modules/home.ts
+++ src/router/modules/home.ts
@@ -2,8 +2,67 @@ import { RouteRecordRaw } from 'vue-router';
 
 export default {
     path: '/',
-    name: 'HomePage',
-    component: () => import('@/views/home/index.vue'),
-    meta: {},
-    children: []
+    name: 'Layout',
+    component: () => import('@/layout/index.vue'),
+    meta: {
+        parentRouter: ''
+    },
+    redirect: '/home',
+    children: [
+        {
+            path: '/home',
+            name: 'HomePage',
+            component: () => import('@/views/home/index.vue'),
+            meta: {
+                isMenu: true,
+                title: '默认首页',
+                parentRouter: 'Layout'
+            },
+            children: []
+        },
+        {
+            path: '/parent',
+            name: 'ParentPage',
+            meta: {
+                isMenu: true,
+                title: '父级菜单',
+                parentRouter: 'Layout'
+            },
+            children: [
+                {
+                    path: '/parent/child1',
+                    name: 'ChildPage1',
+                    component: () => import(/* webpackChunkName: "project" */ '@/views/parent/child-one.vue'), // 内联注释，实现懒加载
+                    meta: {
+                        isMenu: true,
+                        title: '子菜单1',
+                        parentRouter: 'ParentPage'
+                    },
+                    children: []
+                },
+                {
+                    path: '/parent/child2',
+                    name: 'ChildPage2',
+                    component: () => import(/* webpackChunkName: "project" */ '@/views/parent/child-two.vue'), // 内联注释，实现懒加载
+                    meta: {
+                        isMenu: true,
+                        title: '子菜单2',
+                        parentRouter: 'ParentPage'
+                    },
+                    children: []
+                },
+                {
+                    path: '/parent/child3',
+                    name: 'ChildPage3',
+                    component: () => import(/* webpackChunkName: "project" */ '@/views/parent/child-three.vue'), // 内联注释，实现懒加载
+                    meta: {
+                        isMenu: true,
+                        title: '子菜单3',
+                        parentRouter: 'ParentPage'
+                    },
+                    children: []
+                }
+            ]
+        }
+    ]
 } as RouteRecordRaw;
